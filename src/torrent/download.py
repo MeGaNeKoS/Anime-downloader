3e895a439eaabbd3bd3c9b4be48d35cc90fb8cf6
MeGaNeKo <evictory90@gmail.com>
2022-06-26 07:49:41 +0700

    feat: download a torrent from the queue to the torrent client

--- src/torrent/download.py
+++ src/torrent/download.py
@@ -21,6 +21,26 @@ def connect():
     )
 
 
+def add_torrent(num_retries=10):
+    anime = next(iter(queue))
+    download = queue.pop(anime)
+    for i in range(num_retries):
+        if qbt_client.torrents_add(download["link"], tags=config.CLIENT_TAG, category=anime) == "Ok.":
+            break
+    else:
+        logger.error(f"Failed to add torrent\n{download}")
+        queue[anime] = download
+        return
+    datas = qbt_client.torrents_info(category=anime)
+    if len(datas) != 1:
+        logger.error("More than one torrent with category anime_id")
+    data = datas[0]
+    download["hash"] = data['hash']
+    download["status"] = "downloading"
+
+    downloads[anime] = download
+
+
 def remove(anime_id):
     # cancel the download, delete the local files,
     # and add the anime to the log file

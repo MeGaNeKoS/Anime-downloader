46f2c6cdd044ab44525c738a69a999e6cf39e3a1
MeGaNeKo <evictory90@gmail.com>
2022-06-29 07:14:34 +0700

    feat: start flow for rss watcher

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import time
 import traceback
 
@@ -6,27 +7,50 @@ import devlog
 
 from src import config
 from src.torrent import rss
+from src.share_var import logs, lock
 
 
 @devlog.log_on_error(trace_stack=True)
-def main():
+def start_rss():
     while True:
 
         for link in config.RSS_LIST:
-            log_file = f'{config.DATA_DIR}/log/{link.partition("q=")[2]}.txt'
-            try:
-                with open(log_file, 'r') as f:
-                    log = f.read().splitlines()
-            except FileNotFoundError:
-                with open(log_file, 'w+') as f:
-                    pass
-                log = []
-            with open(f'{config.DATA_DIR}/ignore.txt', 'r') as f:
-                ignore = f.read().splitlines()
+            print(link)
+            query = link.partition("q=")[2]
+            log_file = f'{config.DATA_DIR}/log/{query}.txt'
+            # using os module, check if the file exists
+            if not os.path.isfile(log_file):
+                # if not, create with folder
+                os.makedirs(os.path.dirname(log_file), exist_ok=True)
 
-            rss.rss(link, ignore, log)
-        print("All rss downloaded. wait 10 minute for recheck")
+            with lock:
+                file_log = logs.get(query, [])
+
+            with open(f'{config.DATA_DIR}/ignore.txt', 'r+') as input_file:
+                ignore = input_file.read().splitlines()
+
+            rss.rss(link, ignore, file_log)
+            with lock:
+                with open(log_file, 'w+', encoding="utf-8") as output:
+                    for magnet_link in file_log:
+                        output.write("%s\n" % magnet_link)
+            print(f"All rss downloaded. wait {config.CHECK_INTERVAL // 60} minute for recheck")
         time.sleep(config.CHECK_INTERVAL)
+
+
+def start_qbt():
+    # start qbt download manager
+    pass
+
+
+def initialize_gdrive():
+    # make sure we log in to gdrive first
+    pass
+
+
+def main():
+    # creating the thread
+    pass
 
 
 if __name__ == '__main__':
--- src/share_var.py
+++ src/share_var.py
@@ -1,5 +1,11 @@
+from src import config
 import threading
 
 queue = {}
 downloads = {}
 lock = threading.Lock()
+logs = {}
+
+for link in config.RSS_LIST:
+    query = link.partition("q=")[2]
+    logs[query] = []
